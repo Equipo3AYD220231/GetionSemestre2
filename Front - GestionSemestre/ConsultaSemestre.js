f871c1665d51b67b7bbbf746fb7bf9b76cfd45e9
Adrian Espinosa <mauricio.espinosa@udea.edu.co>
2023-04-29 18:07:45 -0500

    semi conexion con el front, funciona GEt y POST, errores en sss de los nombres

--- Front - GestionSemestre/ConsultaSemestre.js
+++ Front - GestionSemestre/ConsultaSemestre.js
@@ -3,7 +3,7 @@ const btnConsultarSemestres = document.getElementById('btn-consultar-semestres')
 const listaSemetres = document.getElementById('lista-semestres');
 
 btnConsultarSemestres.addEventListener('click', () => {
-  fetch('http://localhost:8080/semestres')
+  fetch('http://localhost:8080/semesters')
     .then(response => {
       if (response.ok) {
         return response.json();
@@ -14,103 +14,47 @@ btnConsultarSemestres.addEventListener('click', () => {
     .then(semestres => {
       listaSemetres.innerHTML = ''; // Limpiar la lista de tareas
       semestres.forEach(semestre => {
+       //console.log(semestre);
         const li = document.createElement('li');
-        li.textContent = `${semestre.id} - ${semestre.title} - ${semestre.description}- ${semestre.date}`;
-        listaTareas.appendChild(li);
+        li.textContent = `${semestre.id} - ${semestre.fechainicial} - ${semestre.fechafinal}- ${semestre.evaluacionFinal} - ${semestre.habilitacion} - ${semestre.evaluacion40}`;
+        listaSemetres.appendChild(li);
       });
     })
     .catch(error => {
       console.error(error);
-      alert('Ocurrió un error al obtener la lista de tareas');
+      alert('Ocurrió un error al mostrar tablas de la lista de tareas');
     });
 });
 
+/****  Tratamiento respuesta POST       ***** */
+const form = document.querySelector('#form-anadir-semestre');
+ form.addEventListener('submit', (event) => {
+ event.preventDefault(); // evitar que el formulario se envíe por defecto
 
-/*
+  // obtener los valores ingresados en el formulario
+  const fechainicial = document.querySelector('#fecha-inicio').value;
+  const fechafinal = document.querySelector('#fecha-fin').value;
+  const evaluacionFinal = document.querySelector('#evaluacion-final').value;
+  const evaluacion40 = document.querySelector('#evaluacion-40').value;
+  const habilitacion = document.querySelector('#habilitacion').value;
 
+  // crear el objeto data con los valores del formulario
+  const postData = {
+    fechainicial : fechainicial,
+    fechafinal : fechafinal,
+    evaluacionFinal : evaluacionFinal,
+    habilitacion : habilitacion,
+    evaluacion40 : evaluacion40,
+       };
 
-
-const miBoton = document.getElementById('mi-boton');
-function cargarSemestres() {
-    // limpiar tabla
-    document.getElementById("tbody-semestres").innerHTML = "";
-   
-  
-    // obtener los semestres de la API
-    fetch("http://localhost:8080/semesters" , {
-      method: "GET"  })
-      .then((response) => response.json())
-      .then((data) => {
-        // recorrer los semestres y agregarlos a la tabla
-        data.forEach((semesters) => {
-          let row = `<tr>
-                          <td>${semester.id}</td>
-                          <td>${semester.fechainicial}</td>
-                          <td>${semester.fechafinal}</td>
-                          <td>${semester.evaluacion40}</td>
-                          <td>${semester.evaluacionFinal}</td>
-                          <td>${semester.habilitacion}</td>
-                          <td>
-                              <button class="btn btn-primary btn-sm" data-toggle="modal" data-target="#editarSemestreModal" onclick="editarSemestre(${semester.id})">
-                                  Editar
-                              </button>
-                              <button class="btn btn-danger btn-sm" onclick="eliminarSemestre(${semester.id})">
-                                  Eliminar
-                              </button>
-                          </td>
-                      </tr>`;
-          document.getElementById("tbody-semestres").innerHTML += row;
-        });
-      })
-      .catch((error) => console.log(error));
- 
-    }
-    miBoton.addEventListener('click', cargarSemestres);
-    console.log("hola el boton funciona");
-
-    */
-
-// fiNAL función para cargar los semestres en la tabla
-
-  // función para agregar un nuevo semestre
- /* function agregarSemestre() {
-    // obtener datos del formulario
-    let fechainicial = document.getElementById("fechainicial").value;
-    let fechafinal = document.getElementById("fechafinal").value;
-    let evaluacion40 = document.getElementById("evaluacion40").value;
-    let evaluacionFinal = document.getElementById("evaluacionFinal").value;
-    let habilitacion = document.getElementById("habilitacion").value;
-  
-    // crear objeto semestre
-    let semestre = {
-      fechainicial: fechainicial,
-      fechafinal: fechafinal,
-      evaluacion40: evaluacion40,
-      evaluacionFinal: evaluacionFinal,
-      habilitacion: habilitacion,
-    };
-  
-    // enviar semestre a la API
-    fetch("/savesemester", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(semestre),
+     fetch('http://localhost:8080/savesemester', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json'  },
+      body: JSON.stringify(postData)
     })
-      .then((response) => {
-        if (response.ok) {
-          // limpiar formulario
-          document.getElementById("form-agregar-semestre").reset();
-          // cerrar modal
-          $("#agregarSemestreModal").modal("hide");
-          // recargar tabla
-          cargarSemestres();
-        } else {
-          throw new Error("Error al agregar semestre");
-        }
-      })
-      .catch((error) => console.log(error));
-  }
+      .then(response => response.json()) 
+      .then(json => console.log(json))
+      .catch(err => console.log(err));
   
- */
+  });
+  
